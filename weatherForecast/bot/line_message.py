ce9386c1160d6d2855ca2f2dc32b3e96a42d3a40
nobuhito0906 <77920457+nobuhito0906@users.noreply.github.com>
2022-03-15 23:35:35 +0900

    Update line_message.py
    環境変数取得箇所を修正

--- weatherForecast/bot/line_message.py
+++ weatherForecast/bot/line_message.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import urllib.request
 import json
+import os
 
 REPLY_ENDPOINT_URL = "https://api.line.me/v2/bot/message/reply"
-ACCESS_TOKEN = ["ACCESS_TOKEN"]
+ACCESS_TOKEN = os.getenv["ACCESS_TOKEN"]
 HEADER = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer'+ACCESS_TOKEN
@@ -16,13 +17,14 @@ class LineMessage():
     def __init__(self, messages):
         self.messages = messages
 
-    def reply(self,reply_token):
-        body={
-            'replyToken':reply_token,
-            'messages':self.messages
+    def reply(self, reply_token):
+        body = {
+            'replyToken': reply_token,
+            'messages': self.messages
         }
         print(body)
-        req =urllib.request.Request(REPLY_ENDPOINT_URL,json.dumps(body).encode(),HEADER)
+        req = urllib.request.Request(
+            REPLY_ENDPOINT_URL, json.dumps(body).encode(), HEADER)
         try:
             with urllib.request.urlopen(req) as res:
                 body = res.read()
@@ -30,5 +32,3 @@ class LineMessage():
             print(err)
         except urllib.error.URLError as err:
             print(err.reason)
-            
-    
