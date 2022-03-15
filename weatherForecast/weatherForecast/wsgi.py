859bda225f7a3cb348280df233dac3929615eb60
nobuhito0906 <77920457+nobuhito0906@users.noreply.github.com>
2022-03-15 22:20:10 +0900

    Update wsgi.py

--- weatherForecast/weatherForecast/wsgi.py
+++ weatherForecast/weatherForecast/wsgi.py
@@ -12,6 +12,10 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherForecast.settings')
 
 application = Cling(get_wsgi_application())
