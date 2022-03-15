d68a5e04575c1e4f5fe3a54784fa05467d8e7de3
nobuhito0906 <77920457+nobuhito0906@users.noreply.github.com>
2022-03-15 22:32:46 +0900

    Update settings.py

--- weatherForecast/weatherForecast/settings.py
+++ weatherForecast/weatherForecast/settings.py
@@ -84,16 +84,16 @@ WSGI_APPLICATION = 'weatherForecast.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-import dj_database_url
+# import dj_database_url
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
