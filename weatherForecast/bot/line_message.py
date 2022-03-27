5176a24c1e625ec24d32a3a09aa66812c1a7e3d0
nobuhito0906 <77920457+nobuhito0906@users.noreply.github.com>
2022-03-27 15:37:50 +0900

    天気予報API呼び出し

--- weatherForecast/bot/line_message.py
+++ weatherForecast/bot/line_message.py
@@ -1,3 +1,4 @@
+import json
 from django.http import HttpResponse
 import os
 from linebot import (
@@ -9,6 +10,7 @@ from linebot.models import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
+import datetime
 
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 SECRET = os.getenv("CHANNEL_SECRET")
@@ -23,6 +25,7 @@ def reply(data, signature):
     except InvalidSignatureError:
         return HttpResponse("Sigature Error..")
     return HttpResponse("OK")
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handleMessage(event):
@@ -40,16 +43,24 @@ def handleMessage(event):
             TextSendMessage(text=event.message.text)
         )
 
+
 @handler.add(MessageEvent, message=LocationMessage)
 def handleLocale(event):
-    print("type:",event.message.type)
+    print("type:", event.message.type)
     if event.message.type == "location":
         latitude = event.message.latitude
         longitude = event.message.longitude
+        # 現在時刻
+        dt_now = datetime.datetime.now()
+        # Open-Meteoの天気予報API
+        opmtResponse = "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&hourly={}".format(
+            latitude, longitude, dt_now.hour)
+        print("opmtResponse:", opmtResponse)
         message = "緯度:{}\n経度:{}".format(latitude, longitude)
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=message)
+            TextSendMessage(text=message),
+            TextSendMessage(opmtResponse.json())
         )
     else:
         line_bot_api.reply_message(
