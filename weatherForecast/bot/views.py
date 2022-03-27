20b9eab6986a0406300c9eaf21de407eb1e51527
nobuhito0906 <77920457+nobuhito0906@users.noreply.github.com>
2022-03-27 14:29:03 +0900

    ビルドエラー修正

--- weatherForecast/bot/views.py
+++ weatherForecast/bot/views.py
@@ -8,7 +8,7 @@ def index(request):
     if request.method == 'POST':
         request = json.loads(request.body.decode('utf-8'))
         print("req:",request)
-        signature = request.headers['X-Line-Signature']
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
         data = request['events'][0]
         print("data:",data)
         line_message.reply(data, signature)
